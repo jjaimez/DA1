cfdd2d4ff1e5b9c69813726bb6cc28a213631717
marguet <marguet105@hotmail.com>
2014-09-07 18:32:43 -0500

    int. venta y comp

--- GestionDeNegocio/GestionDeNegocio/realizarCompra.Designer.cs
+++ GestionDeNegocio/GestionDeNegocio/realizarCompra.Designer.cs
@@ -28,11 +28,382 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Cod = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNom = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Fram = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtFram = new System.Windows.Forms.TextBox();
+            this.dgvArt = new System.Windows.Forms.DataGridView();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Codigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Marca = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Descripcion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgvCompra = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtProv = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtFecha = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnBorrarArtSel = new System.Windows.Forms.Button();
+            this.cbAbona = new System.Windows.Forms.CheckBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvArt)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCompra)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtNom);
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 69);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(327, 117);
+            this.panel1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Cod,
+            this.Nombre});
+            this.dataGridView1.Location = new System.Drawing.Point(40, 31);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 76);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Cod
+            // 
+            this.Cod.HeaderText = "Cod";
+            this.Cod.Name = "Cod";
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(14, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Proveedores";
+            // 
+            // txtNom
+            // 
+            this.txtNom.Location = new System.Drawing.Point(96, 6);
+            this.txtNom.Name = "txtNom";
+            this.txtNom.Size = new System.Drawing.Size(184, 20);
+            this.txtNom.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.dgvArt);
+            this.panel2.Controls.Add(this.txtFram);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.Fram);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Location = new System.Drawing.Point(12, 222);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(327, 179);
+            this.panel2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(14, 198);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Artículos";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Codigo";
+            // 
+            // Fram
+            // 
+            this.Fram.AutoSize = true;
+            this.Fram.Location = new System.Drawing.Point(14, 40);
+            this.Fram.Name = "Fram";
+            this.Fram.Size = new System.Drawing.Size(30, 13);
+            this.Fram.TabIndex = 1;
+            this.Fram.Text = "Fram";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(60, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(198, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // txtFram
+            // 
+            this.txtFram.Location = new System.Drawing.Point(60, 37);
+            this.txtFram.Name = "txtFram";
+            this.txtFram.Size = new System.Drawing.Size(198, 20);
+            this.txtFram.TabIndex = 3;
+            // 
+            // dgvArt
+            // 
+            this.dgvArt.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvArt.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.Codigo,
+            this.Marca,
+            this.Descripcion});
+            this.dgvArt.Location = new System.Drawing.Point(18, 63);
+            this.dgvArt.Name = "dgvArt";
+            this.dgvArt.Size = new System.Drawing.Size(296, 101);
+            this.dgvArt.TabIndex = 4;
+            // 
+            // Id
+            // 
+            this.Id.HeaderText = "Id";
+            this.Id.Name = "Id";
+            // 
+            // Codigo
+            // 
+            this.Codigo.HeaderText = "Codigo";
+            this.Codigo.Name = "Codigo";
+            // 
+            // Marca
+            // 
+            this.Marca.HeaderText = "Marca";
+            this.Marca.Name = "Marca";
+            // 
+            // Descripcion
+            // 
+            this.Descripcion.HeaderText = "Descripcion";
+            this.Descripcion.Name = "Descripcion";
+            // 
+            // dgvCompra
+            // 
+            this.dgvCompra.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCompra.Location = new System.Drawing.Point(14, 42);
+            this.dgvCompra.Name = "dgvCompra";
+            this.dgvCompra.Size = new System.Drawing.Size(443, 195);
+            this.dgvCompra.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(349, 46);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 20);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Compra";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 19);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Proveedor";
+            // 
+            // txtProv
+            // 
+            this.txtProv.Location = new System.Drawing.Point(78, 12);
+            this.txtProv.Name = "txtProv";
+            this.txtProv.Size = new System.Drawing.Size(153, 20);
+            this.txtProv.TabIndex = 8;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(248, 17);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(37, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Fecha";
+            // 
+            // txtFecha
+            // 
+            this.txtFecha.Location = new System.Drawing.Point(291, 10);
+            this.txtFecha.Name = "txtFecha";
+            this.txtFecha.Size = new System.Drawing.Size(135, 20);
+            this.txtFecha.TabIndex = 10;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label7);
+            this.panel3.Controls.Add(this.txtFecha);
+            this.panel3.Controls.Add(this.dgvCompra);
+            this.panel3.Controls.Add(this.label6);
+            this.panel3.Controls.Add(this.txtProv);
+            this.panel3.Location = new System.Drawing.Point(345, 69);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(467, 252);
+            this.panel3.TabIndex = 11;
+            // 
+            // btnBorrarArtSel
+            // 
+            this.btnBorrarArtSel.Location = new System.Drawing.Point(345, 327);
+            this.btnBorrarArtSel.Name = "btnBorrarArtSel";
+            this.btnBorrarArtSel.Size = new System.Drawing.Size(179, 30);
+            this.btnBorrarArtSel.TabIndex = 12;
+            this.btnBorrarArtSel.Text = "Borrar Articulos Seleccinados";
+            this.btnBorrarArtSel.UseVisualStyleBackColor = true;
+            // 
+            // cbAbona
+            // 
+            this.cbAbona.AutoSize = true;
+            this.cbAbona.Location = new System.Drawing.Point(550, 335);
+            this.cbAbona.Name = "cbAbona";
+            this.cbAbona.Size = new System.Drawing.Size(57, 17);
+            this.cbAbona.TabIndex = 14;
+            this.cbAbona.Text = "Abona";
+            this.cbAbona.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(665, 333);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(31, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Total";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(711, 329);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(100, 20);
+            this.txtTotal.TabIndex = 16;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(345, 370);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(213, 41);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(596, 370);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(213, 41);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(320, 12);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(148, 33);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "COMPRA";
+            // 
+            // realizarCompra
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "realizarCompra";
+            this.ClientSize = new System.Drawing.Size(824, 431);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.cbAbona);
+            this.Controls.Add(this.btnBorrarArtSel);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Name = "realizarCompra";
+            this.Text = "Registrar Compra";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvArt)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCompra)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtNom;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Cod;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dgvArt;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Codigo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Marca;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Descripcion;
+        private System.Windows.Forms.TextBox txtFram;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label Fram;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dgvCompra;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtProv;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtFecha;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnBorrarArtSel;
+        private System.Windows.Forms.CheckBox cbAbona;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label9;
     }
 }
--- GestionDeNegocio/GestionDeNegocio/realizarCompra.cs
+++ GestionDeNegocio/GestionDeNegocio/realizarCompra.cs
@@ -16,5 +16,20 @@ namespace GestionDeNegocio
         {
             InitializeComponent();
         }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
